3a510a2ae6773b4376af834371e3dbcf455c029d
MAKIENIPART <20241506@nbsc.edu.ph>
2025-02-24 03:27:34 -0800

    added java
    added java dodong

---
+++ Baldesco_lab2/src/baldesco_lab2/Baldesco_lab2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baldesco_lab2;
+
+/**
+ *
+ * @author PC
+ */
+public class Baldesco_lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Baldesco_lab2/src/prelim/baldesco_lab2_activities.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prelim;
+
+/**
+ *
+ * @author PC
+ */
+public class baldesco_lab2_activities {
+    
+}
