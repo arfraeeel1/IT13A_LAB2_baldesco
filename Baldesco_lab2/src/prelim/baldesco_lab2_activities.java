bdf196a1b0402e1da01b4a16f31b8a7ea8072da3
MAKIENIPART <20241506@nbsc.edu.ph>
2025-02-24 03:35:52 -0800

    added java
    java new activity

--- Baldesco_lab2/src/prelim/baldesco_lab2_activities.java
+++ Baldesco_lab2/src/prelim/baldesco_lab2_activities.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package prelim;
 
-/**
- *
- * @author PC
- */
+package prelim;
+import java.util.Scanner;
+
 public class baldesco_lab2_activities {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+      
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+        
+       
+        int greatest = Math.max(num1, Math.max(num2, num3));
+        
+        
+        System.out.println("The greatest number is: " + greatest);
+        
+        scanner.close();
+    }
 }
+
+
+
+
